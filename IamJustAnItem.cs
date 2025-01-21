67f0d5f36ae5d9e286a9c9d185dbbc7a3fbdefe0
Ilian Petrov <ilianpetrov.personal@gmail.com>
2025-01-21 17:36:10 +0100

    Uploading "Player Items", "Player Chores" and added information to "Player Interaction"
    Maria wrote both the "Player Items" and "Player Chores" and added the necessary code in "Player Interaction". I helped with the "Player Items" and "Player Interacion" when needed.

---
+++ IamJustAnItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IamJustAnItem : MonoBehaviour
+{
+    public string itemName;
+    public Animator clip; //The potential animator for the upgrading the item
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayerChores.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+/*
+ * 
+ */
+
+public class PlayerChores : MonoBehaviour
+{
+    
+    [TextArea(1, 5)]
+    [SerializeField] string info = "This script must be attached to an empty game object, for each day there is a different scene and you need to fill in the tasks of each day in the inspector each, day will start with the first task from the array";
+    private int taskTracker; //this is a variable to keep track of the number of the tasks
+
+    private int[] taskChecker = new int[3]; //this is a 1D array to keep track if the tasks are done: 0 for false, 1 for true
+
+    [SerializeField] string[] playerChores = new string[3]; //list the chores in order in the inspector
+
+    [SerializeField] TMP_Text quest; //the text shown in the UI, the task basically
+
+    public bool[] quest_bool;
+    void Start()
+    {
+        quest.text = playerChores[0]; //shows the first task on the screen
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void TaskIsDone()
+    {
+        taskChecker[taskTracker] = 1;
+    }
+
+    public void ChangeTask()
+    {
+        quest.text = playerChores[++taskTracker];
+    }
+}
